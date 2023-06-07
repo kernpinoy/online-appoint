b12616f0eeef8625950a3781c33e75227e823bdd
Binibini10 <toquejeanelle@gmail.com>
2023-06-07 12:45:14 +0800

    Added new rule in SQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,5 @@
 -- create root user and grant rights
 CREATE USER 'root' IDENTIFIED BY 'root';
 GRANT ALL ON *.* TO 'root';
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
