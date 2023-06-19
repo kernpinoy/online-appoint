e7c33b2d24e1b504e33f5d909568532cf04c3f22
Binibini10 <toquejeanelle@gmail.com>
2023-06-20 02:13:40 +0800

    Added more headers in car_num.php

--- src/api/appointments/car_num.php
+++ src/api/appointments/car_num.php
@@ -4,6 +4,9 @@ include_once "../../class/appointments.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $database = new Database();
 $db = $database->get_connection();
