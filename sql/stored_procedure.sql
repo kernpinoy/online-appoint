97bc99c36791984baad2c9cd745aa1b07f041ec1
Binibini10 <toquejeanelle@gmail.com>
2023-06-20 00:34:37 +0800

    Refactoring SQL: Separeted stored procedure from init.sql

---
+++ sql/stored_procedure.sql
@@ -0,0 +1,75 @@
+USE `online_appoint`;
+
+DELIMITER $$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `delete_appointment`(IN appointment_id INT)
+BEGIN
+    SET SQL_SAFE_UPDATES = 0;
+    
+    DELETE FROM `online_appoint`.`appointments` WHERE `id` = appointment_id;
+
+    SET @count = 0;
+    UPDATE `online_appoint`.`appointments` SET `id` = @count:= @count + 1;
+    ALTER TABLE `online_appoint`.`appointments` AUTO_INCREMENT = 1;
+    
+    SET SQL_SAFE_UPDATES = 1;
+END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `get_appointments`()
+BEGIN
+    SELECT * FROM `online_appoint`.`appointments`;
+END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `get_single_appointment`(IN appointment_id INT)
+BEGIN
+    SELECT * FROM `online_appoint`.`appointments` WHERE `id` = appointment_id;
+END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `update_appointment`(IN p_appointment_id INT, IN p_full_name VARCHAR(100), IN p_office_to_visit VARCHAR(50), IN p_person_to_visit VARCHAR(50), IN p_purpose VARCHAR(50), IN p_with_vehicle INT, IN p_plate_num VARCHAR(50), IN p_time_of_visit VARCHAR(100), IN p_email_address VARCHAR(50))
+BEGIN
+    UPDATE `online_appoint`.`appointments`
+    SET `full_name` = p_full_name,
+        `office_to_visit` = p_office_to_visit,
+        `person_to_visit` = p_person_to_visit,
+        `purpose` = p_purpose,
+        `with_vehicle` = p_with_vehicle,
+        `plate_num` = p_plate_num,
+        `time_of_visit` = p_time_of_visit,
+        `email_address` = p_email_address
+    WHERE `id` = p_appointment_id;
+END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `insert_appointment`(IN p_full_name VARCHAR(100), IN p_office_to_visit VARCHAR(50), IN p_person_to_visit VARCHAR(50), IN p_purpose VARCHAR(50), IN p_with_vehicle INT, IN p_plate_num VARCHAR(50), IN p_time_of_visit VARCHAR(100), IN p_email_address VARCHAR(50))
+BEGIN
+    DECLARE last_id INT DEFAULT 0;
+
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- Error occurred during insertion
+        SET last_id = 0;
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Failed to insert appointment.';
+    END;
+
+    START TRANSACTION;
+    
+    -- Insert the appointment
+    INSERT INTO `online_appoint`.`appointments` (`full_name`, `office_to_visit`, `person_to_visit`, `purpose`, `with_vehicle`, `plate_num`, `time_of_visit`, `email_address`)
+    VALUES (p_full_name, p_office_to_visit, p_person_to_visit, p_purpose, p_with_vehicle, p_plate_num, p_time_of_visit, p_email_address);
+    
+    -- Get the last inserted ID
+    SET last_id = LAST_INSERT_ID();
+
+    -- Check if an error occurred during insertion
+    IF last_id = 0 THEN
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Failed to insert appointment.';
+    END IF;
+
+    -- Commit the transaction
+    COMMIT;
+END$$
+
+DELIMITER ;
